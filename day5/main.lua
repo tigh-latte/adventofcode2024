d21519f6611a036bc33994f8070f056be1cd46c3
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-12-05 16:22:47 +0000

    part 1 done

--- day5/main.lua
+++ day5/main.lua
@@ -1,39 +1,32 @@
 #!/usr/bin/env luajit
 
----@class Node
----@field next table<integer, Node>
----@field value integer
-local Node = {}
-Node.__index = Node
+---@alias Rules table<string, boolean>
+
+local input = arg[1] or "./day5/input.txt"
+
+---@param name string
+---@param rules Rules
+local function debug_map(name, rules)
+	for page, rule in pairs(rules) do
+		print(name, page, "->", rule)
+	end
+end
 
 
----@alias Rules table<integer, integer[]>
 
-local input = arg[2] or "./day5/demo.txt"
 
----@return Rules, Rules, integer[][]
+---@return Rules,  integer[][]
 local function read_file()
 	local f = io.open(input, "r")
 	if not f then error("failed to open input", 2) end
 
-	local before, after = {}, {}
+	local rules = {}
 
 	for line in f:lines() do
 		line = line --[[@as string]]
 		if line == "" then break end
 
-		local l, r = line:match("(%d+)|(%d+)")
-		l, r = tonumber(l), tonumber(r)
-
-		if not before[l] then
-			before[l] = {}
-		end
-		table.insert(before[l], r)
-
-		if not after[r] then
-			after[r] = {}
-		end
-		table.insert(after[r], l)
+		rules[line] = true
 	end
 
 	local updates = {}
@@ -49,7 +42,7 @@ local function read_file()
 
 	f:close()
 
-	return before, after, updates
+	return rules, updates
 end
 
 ---@param pages integer[]
@@ -62,77 +55,50 @@ local function debug_list(pages)
 	return s .. "]"
 end
 
----@param name string
----@param rules Rules
-local function debug_map(name, rules)
-	for page, rule in pairs(rules) do
-		print(name, page, "->", debug_list(rule))
-	end
-end
-
-
----@param vv any[]
----@param v any
----@param page any
-local function contains(vv, v, page)
-	for val in pairs(vv) do
-		if val == page then goto continue end
-		print("comparing", val, v)
-		if val == v then return true end
-		::continue::
-	end
-
-	return false
-end
-
----@param before integer[]
----@param after integer[]
----@param visited table<integer, boolean>
----@param update integer[]
----@param page integer
----@return boolean
-local function is_valid_page_update(before, after, visited, update, page)
-	print("page", page)
-	print("after", debug_list(after))
-	if before then
-		for _, b in ipairs(before) do
-			if b ~= page then
-				if contains(update, b, page) and visited[b] then
-					return false
-				end
-			end
-		end
-	end
-	if after then
-		for _, a in ipairs(after) do
-			if a ~= page then
-				local con = contains(update, a, page)
-				if con and not visited[a] then
-					return false
-				end
-			end
-		end
-	end
-	return true
-end
-
 local function part1()
-	local before, after, updates = read_file()
+	local rules, updates = read_file()
 
 	local tally = 0
 
 	for _, update in ipairs(updates) do
-		local visited = {}
 		local all_valid = true
-		for _, page in ipairs(update) do
-			if not is_valid_page_update(before[page], after[page], update, visited, page) then
+
+		print("list", debug_list(update))
+		for i = 1, #update do
+			local after = { unpack(update, i + 1, #update) }
+			local before = { unpack(update, 1, i - 1) }
+
+			local page = update[i]
+
+			print("before", debug_list(before))
+
+			local found = true
+
+			if after and #after > 0 then
+				for _, a in ipairs(after) do
+					local rule = page .. "|" .. a
+					if not rules[rule] then
+						found = false
+						break
+					end
+				end
+			end
+			if before and #before > 0 then
+				for _, b in ipairs(before) do
+					local rule = b .. "|" .. page
+					if not rules[rule] then
+						found = false
+						break
+					end
+				end
+			end
+
+			if not found then
 				all_valid = false
 				break
 			end
-			visited[page] = true
 		end
 		if all_valid then
-			print("valid", debug_list(update))
 			tally = tally + update[math.ceil(#update / 2)]
 		end
 	end
